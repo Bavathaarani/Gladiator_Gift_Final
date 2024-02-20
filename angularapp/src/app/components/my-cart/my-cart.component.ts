0b16641379f9aa630053357d28d643de91fb849e
neojarvis <techadmin@iamneo.ai>
2024-02-20 16:18:24 +0000

    Auto-save-commit-2/20/2024--9:48:23-PM

--- angularapp/src/app/components/my-cart/my-cart.component.ts
+++ angularapp/src/app/components/my-cart/my-cart.component.ts
@@ -31,15 +31,12 @@ export class MyCartComponent implements OnInit {
           // Check if the gifts property is an object with $values array
           if (Array.isArray(response.gifts.$values)) {
             // Extract the array from the nested object
-            // this.gifts = response.gifts.$values;
-            this.gifts = Array.isArray(response.gifts.$values) ? response.gifts.$values : response.gifts;
-          this.totalAmount = response.totalAmount;
+            this.gifts = response.gifts.$values;
+          } else {
+            // If $values array is not present, consider gifts itself as the array
+            this.gifts = response.gifts;
           }
-          // } else {
-          //   // If $values array is not present, consider gifts itself as the array
-          //   this.gifts = response.gifts;
-          // }
-          // this.totalAmount = response.totalAmount;
+          this.totalAmount = response.totalAmount;
           // this.gifts.forEach(gift => {
           //   gift.quantity = 1;
           // });
@@ -56,16 +53,14 @@ export class MyCartComponent implements OnInit {
   
   
 
-  // calculateTotalAmount(): number {
-  //   let totalAmount = 0;
-  //   for (const gift of this.gifts) {
-  //     totalAmount += this.userQuantity * gift.giftPrice;
-  //   }
-  //   return totalAmount;
-  // }
   calculateTotalAmount(): number {
-    return this.gifts.reduce((total, gift) => total + (gift.quantity * gift.giftPrice), 0);
+    let totalAmount = 0;
+    for (const gift of this.gifts) {
+      totalAmount += this.userQuantity * gift.giftPrice;
+    }
+    return totalAmount;
   }
+
   
   // initializeQuantity() {
   //   this.gifts.forEach(giftData => {
@@ -80,18 +75,12 @@ export class MyCartComponent implements OnInit {
   }
 
 
-  // updateQuantity(giftData: any): void {
-  //   if (giftData.quantity > this.maxQuantity) {
-  //     giftData.quantity = this.maxQuantity;
-  //   }
-    updateQuantity(giftData: any): void {
-      giftData.totalAmount = giftData.quantity * giftData.giftPrice;
-      this.updateTotalAmount();
+  updateQuantity(giftData: any): void {
+    if (giftData.quantity > this.maxQuantity) {
+      giftData.quantity = this.maxQuantity;
     }
-    updateTotalAmount(): void {
-      this.totalAmount = this.calculateTotalAmount();
-    }
-    giftData.totalAmount = this.userQuantity * giftData.giftPrice; // Update the total amount for the specific gift
+    
+    giftData.totalAmount = this.maxQuantity * giftData.giftPrice; // Update the total amount for the specific gift
     
     const customerId = localStorage.getItem('customerId'); // Get the customerId from local storage
     
