63d9ae34723636dd04270976c0f5937bbf0727c4
neojarvis <techadmin@iamneo.ai>
2024-02-21 03:38:26 +0000

    Auto-save-commit-21/02/2024--09:08:22

--- dotnetapp/Models/User.cs
+++ dotnetapp/Models/User.cs
@@ -7,10 +7,10 @@ public class User
 {
     [Key]
     public long UserId { get; set; }
-    public string ?Email { get; set; }
-    public string ?Password { get; set; }
-    public string ?Username { get; set; }
-    public string ?MobileNumber { get; set; }
-    public string ?Role { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Username { get; set; }
+    public string? MobileNumber { get; set; }
+    public string? Role { get; set; }
 }
 }
