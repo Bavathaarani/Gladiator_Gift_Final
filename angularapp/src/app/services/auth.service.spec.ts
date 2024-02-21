603420aac82bcfadee7294350650fed0b35c9f91
neojarvis <techadmin@iamneo.ai>
2024-02-21 02:18:04 +0000

    Auto-save-commit-21/02/2024--07:47:59

--- angularapp/src/app/services/auth.service.spec.ts
+++ angularapp/src/app/services/auth.service.spec.ts
@@ -48,83 +48,83 @@
   
 // });
 
-// import { TestBed } from '@angular/core/testing';
-// import {
-//   HttpClientTestingModule,
-//   HttpTestingController,
-// } from '@angular/common/http/testing';
-// import { AuthService } from './auth.service';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { AuthService } from './auth.service';
  
-// describe('AuthService', () => {
-//   let service: AuthService;
-//   let httpMock: HttpTestingController;
+describe('AuthService', () => {
+  let service: AuthService;
+  let httpMock: HttpTestingController;
  
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule],
-//       providers: [AuthService],
-//     });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthService],
+    });
  
-//     service = TestBed.inject(AuthService);
-//     httpMock = TestBed.inject(HttpTestingController);
-//   });
+    service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
  
-//   afterEach(() => {
-//     httpMock.verify(); // Ensure that there are no outstanding requests
-//   });
+  afterEach(() => {
+    httpMock.verify(); // Ensure that there are no outstanding requests
+  });
  
-//   fit('Frontend_should_call_the_api_and_register_the_admin_when_register_is_called', () => {
-//     const registerData  = {
-//       username: 'test',
-//       password: 'password',
-//       role: 'Admin',
-//       email: 'test@test.com',
-//       mobileNumber: '1234567890',
-//     };
-//     const response = { id: 1, ...registerData };
+  fit('Frontend_should_call_the_api_and_register_the_admin_when_register_is_called', () => {
+    const registerData  = {
+      username: 'test',
+      password: 'password',
+      role: 'Admin',
+      email: 'test@test.com',
+      mobileNumber: '1234567890',
+    };
+    const response = { id: 1, ...registerData };
  
-//     (service as any).register(registerData).subscribe();
+    (service as any).register(registerData).subscribe();
  
-//     const req = httpMock.expectOne(`${service.apiUrl}/api/register`);
-//     expect(req.request.method).toBe('POST');
-//     expect(req.request.body).toEqual(registerData);
+    const req = httpMock.expectOne(`${service.apiUrl}/api/register`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(registerData);
  
-//     req.flush(response);
-//   });
+    req.flush(response);
+  });
  
-//   fit('Frontend_should_call_the_api_and_register_the_customer_when_register_is_called', () => {
-//     const registerData = {
-//       username: 'test',
-//       password: 'password',
-//       role: 'Customer',
-//       email: 'test@test.com',
-//       mobileNumber: '1234567890',
-//     };
-//     const response = { id: 1, ...registerData };
+  fit('Frontend_should_call_the_api_and_register_the_customer_when_register_is_called', () => {
+    const registerData = {
+      username: 'test',
+      password: 'password',
+      role: 'Customer',
+      email: 'test@test.com',
+      mobileNumber: '1234567890',
+    };
+    const response = { id: 1, ...registerData };
  
-//     (service as any)
-//       .register(
-//         registerData
-//       )
-//       .subscribe();
+    (service as any)
+      .register(
+        registerData
+      )
+      .subscribe();
  
-//     const req = httpMock.expectOne(`${service.apiUrl}/api/register`);
-//     expect(req.request.method).toBe('POST');
-//     expect(req.request.body).toEqual(registerData);
+    const req = httpMock.expectOne(`${service.apiUrl}/api/register`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(registerData);
  
-//     req.flush(response);
-//   });
+    req.flush(response);
+  });
  
-//   fit('Frontend_should_call_the_api_and_authenticate_the_admin_when_login_is_called', () => {
-//     const loginData = { email: 'test@test.com', password: 'password' };
-//     const response = { username: 'test', role: 'Admin', id: 1 };
+  fit('Frontend_should_call_the_api_and_authenticate_the_admin_when_login_is_called', () => {
+    const loginData = { email: 'test@test.com', password: 'password' };
+    const response = { username: 'test', role: 'Admin', id: 1 };
  
-//     (service as any).login(loginData.email, loginData.password).subscribe();
+    (service as any).login(loginData.email, loginData.password).subscribe();
  
-//     const req = httpMock.expectOne(`${service.apiUrl}/api/login`);
-//     expect(req.request.method).toBe('POST');
-//     expect(req.request.body).toEqual(loginData);
+    const req = httpMock.expectOne(`${service.apiUrl}/api/login`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(loginData);
  
-//     req.flush(response);
-//   });
-// });
+    req.flush(response);
+  });
+});
